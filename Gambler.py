3bd5a0df7828180411050bc5e5932671bf68027c
McCoy <mccoybecker@gmail.com>
2018-07-05 15:37:41 -0500

    Updated the Gambler

--- Gambler.py
+++ Gambler.py
@@ -1,20 +1,79 @@
 
+from SlotMachine import SlotMachine
 import numpy as np
-import tensorflow as tf
+import time
+
+# A knowledge-base. The shaky, drunken memory of our gambler. #
+
+class KnowledgeBase:
+
+    def __init__(self,SlotMachine):
+        self.totalNumSlots = SlotMachine.totalNumSlots
+        self.memory = []
+        self.slotRollNumbers = [0 for j in range(SlotMachine.totalNumSlots)]
+        self.slotRewardList = [0 for j in range(SlotMachine.totalNumSlots)]
+
+    def update(self):
+        self.slotRewardList = [sum([i if list.index(i) == j else 0 for list in self.memory for i in list]) for j in range(self.totalNumSlots)]
+
+        for i in range(len(self.slotRollNumbers)):
+            self.slotRollNumbers[i] = sum([1 if (reward > 0 and event.index(reward) == i) else 0 for event in self.memory for reward in event])
 
 # A machine-learning, gunslinging, rough gambler type. #
 
 class Gambler:
 
-    def __init__(self, SlotMachine, Wallet):
+    def __init__(self, SlotMachine, Wallet, Epsilon):
+
         self.Casino = SlotMachine
         self.actions = SlotMachine.actionSpace
         self.Wallet = Wallet
+        self.Epsilon = Epsilon
+        self.KnowledgeBase = KnowledgeBase(SlotMachine)
 
-        # A couple of shots of whiskey, the gambler bets randomly. #
-        self.Policy = [np.random.rand() for _ in range(SlotMachine.totalNumSlots)]
-        self.Bias = np.random.rand()
+    def action(self):
 
+        generateMeanKnowledge = [0 for i in range(self.Casino.totalNumSlots)]
+        for i in range(self.Casino.totalNumSlots):
+            try:
+                generateMeanKnowledge[i] = self.KnowledgeBase.slotRewardList[i]/self.KnowledgeBase.slotRollNumbers[i]
+            except ZeroDivisionError:
+                generateMeanKnowledge[i] = 0
 
-    # Sober the gambler up! #
-    def train(self):
+        print("Here's the current roll list: " + str(self.KnowledgeBase.slotRollNumbers))
+        print("Current estimators of the mean for each slot: " + str(generateMeanKnowledge))
+
+        bestKnown = generateMeanKnowledge.index(max(generateMeanKnowledge))
+        print("The gunslinger will try slot (" + str(bestKnown) + ")")
+
+        #Here is the famous 1-|epsilon| policy
+
+        if np.random.rand(1)<(1-(self.Epsilon/(1+sum(self.KnowledgeBase.slotRollNumbers)))):
+            reward = self.Casino.action(bestKnown)
+        else:
+            action = np.random.randint(0,self.Casino.totalNumSlots)
+            print("The gunslinger is actually going for slot (" + str(action) + ")")
+            reward = self.Casino.action(action)
+
+        self.Wallet += sum(reward)
+        self.KnowledgeBase.memory.append(reward)
+        self.KnowledgeBase.update()
+
+# Happy gambling! #
+
+def main():
+    Casino = SlotMachine(int(input("How many slots do you you want to the gunslinger to play with?: ")))
+    Wallet = int(input("How much money is the gunslinger rolling with? "))
+    GambleCost = 50
+    print("The cost to roll is 50 big ones.")
+    Gunslinger = Gambler(Casino, Wallet, 0.50)
+
+    while Gunslinger.Wallet > 50:
+        Gunslinger.action()
+        Gunslinger.Wallet -= GambleCost
+        print(Gunslinger.Wallet)
+        time.sleep(2)
+
+    print("The Gunslinger is out of cash!")
+
+main()
--- SlotMachine.py
+++ SlotMachine.py
@@ -6,7 +6,7 @@ import numpy as np
 class Slot:
     
     def __init__(self):
-        self.mean = np.random.randint(0,100)
+        self.mean = np.random.randint(45,55)
         self.variance = np.sqrt(self.mean)
 
     def roll(self):
@@ -28,29 +28,4 @@ class SlotMachine:
     def action(self, slotNumber):
         Choice = self.SlotList[slotNumber]
         instantReward = Choice.roll()
-        return(sum([instantReward if i == slotNumber else 0 for i in range(len(self.SlotList))]))
-
-# Happy gambling! #
-
-def main():
-    Casino = SlotMachine(int(input("How many slots do you you want to play with?: ")))
-    Wallet = int(input("How much money are you rolling with? "))
-    GambleCost = 50
-    print("The cost to roll is 50 big ones.")
-    while Wallet > 50:
-
-        print("Here are the slots you can roll at: " + str(Casino.actionSpace))
-        SlotNumber = int(input("Pick a slot: "))
-
-        try:
-            reward = Casino.action(SlotNumber)
-            print("Here are your rewards: " + str(reward))
-            Wallet += reward - GambleCost
-            print("You now have " + str(Wallet) + " to play with.")
-
-        except IndexError:
-            print("You're trying to gamble outside the casino! Pick again partner.")
-
-    print("You don't have enough cash to gamble!")
-
-main()
+        return([instantReward if i == slotNumber else 0 for i in range(len(self.SlotList))])
